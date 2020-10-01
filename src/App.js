012cf66de646d825e6007487a89208704af8d2d2
User-WFH <User-WFH@01hw826921.India.TCS.com>
2020-10-01 12:05:30 +0530

    latest update changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import HomePage from './homepage.component'
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import "./App.css";
 
 function App() {
   return (
-    <HomePage/>
-  )
-  
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
